ae1f0235387129d8ef1eea3ebcbbd4b07f3b6c2d
Van <82447582+varttist@users.noreply.github.com>
2025-07-03 22:47:26 +0300

    change CMMarinesPerXeno ccvar from server to replicated  (#138)

--- Content.Shared/_RMC14/CCVar/RMCCVars.cs
+++ Content.Shared/_RMC14/CCVar/RMCCVars.cs
@@ -34,7 +34,8 @@ public sealed partial class RMCCVars : CVars
         CVarDef.Create("rmc.bleed_time_multiplier", 1f, CVar.REPLICATED | CVar.SERVER);
 
     public static readonly CVarDef<float> CMMarinesPerXeno =
-        CVarDef.Create("rmc.marines_per_xeno", 5f, CVar.SERVER | CVar.SERVERONLY);
+        //CVarDef.Create("rmc.marines_per_xeno", 5f, CVar.SERVER | CVar.SERVERONLY);
+        CVarDef.Create("rmc.marines_per_xeno", 5f, CVar.REPLICATED | CVar.SERVER); // CCM-cvars-perm
 
     public static readonly CVarDef<bool> RMCAutoBalance =
         CVarDef.Create("rmc.auto_balance", true, CVar.SERVER | CVar.SERVERONLY);
