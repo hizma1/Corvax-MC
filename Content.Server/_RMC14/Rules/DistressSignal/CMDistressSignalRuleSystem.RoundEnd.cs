c8a06bfa8fcef489b7e77d38ed0e80e16037c03e
Fenriz <kastonbag552@gmail.com>
2026-04-12 19:48:37 +0300

    upd todo

--- Content.Server/_RMC14/Rules/DistressSignal/CMDistressSignalRuleSystem.RoundEnd.cs
+++ Content.Server/_RMC14/Rules/DistressSignal/CMDistressSignalRuleSystem.RoundEnd.cs
@@ -297,15 +297,7 @@ public sealed partial class CMDistressSignalRuleSystem
         if (!rule.AutoEnd)
             return;
 
-        // you might be wondering what this check is doing here
-        // the answer is simple
-        // the absolute unit that wrote game rule system and game ticker made a conveniently named ActiveTick method
-        // that gets ticked BEFORE THE FUCKING ROUND IS SETUP
-        // so no marines and no xenos means instant mutual annihilation
-        // therefore we wait an arbitrary 1 minute
-        // i fucking hate my life dude
-        // i slept 3 hours and have to stay up to manually end ANOTHER ROUND OF RMC14
-        // TODO RMC14 why are we still here
+        // TODO RMC14: Refactor round end timing
         if (rule.StartTime == null || Timing.CurTime - rule.StartTime < rule.RoundEndCheckDelay)
             return;
 
